e87cbdec1b2e8a8307f0bccd011a0f8a3c1ad462
Quasar-Kim <vhfflrhs128@gmail.com>
2025-11-19 16:15:00 +0900

    test: introduce timeout to FlatSpecBase

--- jobs/src/test/scala/redsort/FlatSpecBase.scala
+++ jobs/src/test/scala/redsort/FlatSpecBase.scala
@@ -5,5 +5,14 @@ import cats.effect.testing.scalatest.AsyncIOSpec
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalamock.stubs.CatsEffectStubs
+import org.scalatest.concurrent.AsyncTimeLimitedTests
+import scala.concurrent.duration._
 
-class FlatSpecBase extends AsyncFlatSpec with AsyncIOSpec with Matchers with CatsEffectStubs
+class FlatSpecBase
+    extends AsyncFlatSpec
+    with AsyncIOSpec
+    with Matchers
+    with CatsEffectStubs
+    with AsyncTimeLimitedTests {
+  val timeLimit = 1.seconds
+}
