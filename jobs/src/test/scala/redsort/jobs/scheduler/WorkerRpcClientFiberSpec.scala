93d0c928ae0fb35e0b748d490dccef4c0bdd4529
Quasar-Kim <vhfflrhs128@gmail.com>
2025-11-19 15:52:01 +0900

    refactor: WorkerRpcClientFiberSpec now use fixtures and shared values

--- jobs/src/test/scala/redsort/jobs/scheduler/WorkerRpcClientFiberSpec.scala
+++ jobs/src/test/scala/redsort/jobs/scheduler/WorkerRpcClientFiberSpec.scala
@@ -13,40 +13,53 @@ import redsort.jobs.messages.JobExecutionStats
 import scala.concurrent.duration._
 
 class WorkerRpcClientFiberSpec extends FlatSpecBase {
-  "Scheduler Fiber" should "call RunJob RPC method on receiving Job event" in {
-    val wid = new Wid(0, 0)
-    val spec = new JobSpec(
-      name = "test",
-      args = Seq(),
-      inputs = Seq(),
-      outputs = Seq(),
-      outputSize = 0
-    )
-    val result = new JobResult(
-      success = true,
-      retval = None,
-      error = None,
-      stats = Some(
-        new JobExecutionStats(
-          calculationTime = 1
+  def fixture =
+    new {
+      val sharedState = SharedState.init(
+        Map(
+          new Wid(0, 0) -> new NetAddr("1.2.3.4", 5000),
+          new Wid(0, 1) -> new NetAddr("1.2.3.4", 5001)
         )
       )
-    )
+      val ctxStub = stub[WorkerRpcClient]
+      val workerfs2GrpcStub = stub[WorkerFs2Grpc[IO, Metadata]]
+      (ctxStub.workerRpcClient _).returnsWith(Resource.eval(IO(workerfs2GrpcStub)))
 
-    val ctx = stub[WorkerRpcClient]
-    val innerStub = stub[WorkerFs2Grpc[IO, Metadata]]
-    (innerStub.runJob _).returnsWith(IO(result))
-    (ctx.workerRpcClient _).returnsWith(Resource.eval(IO(innerStub)))
+      val inputQueue = Queue.unbounded[IO, WorkerFiberEvents]
+    }
+
+  val spec = new JobSpec(
+    name = "test",
+    args = Seq(),
+    inputs = Seq(),
+    outputs = Seq(),
+    outputSize = 0
+  )
+
+  val jobResult = new JobResult(
+    success = true,
+    retval = None,
+    error = None,
+    stats = Some(
+      new JobExecutionStats(
+        calculationTime = 1
+      )
+    )
+  )
+
+  "Scheduler Fiber" should "call RunJob RPC method of worker on receiving Job event" in {
+    val f = fixture
+    (f.workerfs2GrpcStub.runJob _).returnsWith(IO(jobResult))
 
     for {
-      stateR <- SharedState.init(Map(wid -> new NetAddr("1.2.3.4", 5000)))
-      queue <- Queue.unbounded[IO, WorkerFiberEvents]
-      _ <- WorkerRpcClientFiber.start(stateR, wid, queue, ctx).use { _ =>
-        queue.offer(WorkerFiberEvents.Job(spec)) >> IO.sleep(100.milli)
+      stateR <- f.sharedState
+      inputQueue <- f.inputQueue
+      _ <- WorkerRpcClientFiber.start(stateR, new Wid(0, 0), inputQueue, f.ctxStub).use { _ =>
+        inputQueue.offer(WorkerFiberEvents.Job(spec)) >> IO.sleep(100.milli)
       }
     } yield {
       assertResult(JobSpec.toMsg(spec)) {
-        (innerStub.runJob _).calls(0)._1
+        (f.workerfs2GrpcStub.runJob _).calls(0)._1
       }
     }
   }
