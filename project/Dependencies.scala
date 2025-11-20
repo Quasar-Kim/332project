5936f6f6e2fa66e35ceb5a98e0994b7d75e4500f
Quasar-Kim <vhfflrhs128@gmail.com>
2025-11-20 21:20:59 +0900

    feat: add monocle library

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,12 +3,15 @@ import sbt._
 object Dependencies {
     // Versions
     val scalamockVersion = "7.5.2"
+    val monocleVersion = "3.1.0"
     
     // Libraries
     val catsEffect = "org.typelevel" %% "cats-effect" % "3.6.3"
     val log4cats = "org.typelevel" %% "log4cats-slf4j" % "2.7.1"
     val grpcNetty = "io.grpc" % "grpc-netty-shaded" % scalapb.compiler.Version.grpcJavaVersion
     val scalapbRuntime = "com.thesamet.scalapb" %% "scalapb-runtime" % scalapb.compiler.Version.scalapbVersion % "protobuf"
+    val monocleCore = "dev.optics" %% "monocle-core" % monocleVersion
+    val monocleMacro = "dev.optics" %% "monocle-macro" % monocleVersion
 
     // -- testing deps
     val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19" % "test"
@@ -22,6 +25,8 @@ object Dependencies {
         catsEffect,
         log4cats,
         grpcNetty,
+        monocleCore,
+        monocleMacro,
         scalaTest,
         scalactic,
         scalamock,
