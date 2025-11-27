984dc15cc2cbde1678e326a337f3f16e4aba77fa
DanaKharaz <danakharaz@gmail.com>
2025-11-27 20:33:26 +0900

    fix: ensure the order of data is perserved in write

--- jobs/src/main/scala/redsort/jobs/context/impl/ReplicatorRemoteServerImpl.scala
+++ jobs/src/main/scala/redsort/jobs/context/impl/ReplicatorRemoteServerImpl.scala
@@ -4,6 +4,7 @@ import cats.effect.IO
 import com.google.protobuf.ByteString
 import com.google.protobuf.empty.Empty
 import io.grpc.Metadata
+import fs2.Stream
 import redsort.jobs.context.interface.FileStorage
 import redsort.jobs.messages.{Packet, ReadRequest, ReplicatorRemoteServiceFs2Grpc, WriteRequest}
 
@@ -16,22 +17,42 @@ class ReplicatorRemoteServerImpl(fileStorage: FileStorage)
   }
 
   override def write(request: fs2.Stream[IO, WriteRequest], ctx: Metadata): IO[Empty] = {
-    var path = ""
-    request
-      .evalMap { writeRequest =>
-        path = writeRequest.path match {
-          case Some(p) => p
-          case _ => ""
-      }
-        IO.pure(writeRequest.data.toByteArray)
-      }
+    // TODO : fix how file is written (whole file is resource-heavy)
+    // TODO : multiple different paths error?
+    // TODO : file already exists?
+    val fromAllRequests: Stream[IO, Either[(String, Array[Byte]), Array[Byte]]] =
+      request.evalMap { writeRequest =>
+        // array of bytes from the request
+        val bytes = writeRequest.data.toByteArray
+
+        writeRequest.path match {
+          case Some(path) => IO.pure(Left((path, bytes))) // both the path and the bytes
+          case None => IO.pure(Right(bytes)) // only the bytes
+        }
+    }
+
+    // consume the resulting stream
+    fromAllRequests
       .compile
       .toList
-      .map { bytesList =>
-        bytesList.flatten.toArray
-      }
-      .flatMap { fullData =>
-        fileStorage.writeAll(path, fullData)
+      .flatMap { items =>
+        // try to extract the path
+        val maybePath = items.collectFirst { case Left((path, _)) => path }
+
+        maybePath match {
+          case None =>
+            IO.raiseError(new RuntimeException("no path in the stream of write requests")) // TODO : specify error?
+          case Some(path) => {
+            // concatenate the data (should preserve order)
+            val data: Array[Byte] = items.flatMap {
+              case Left((_, bytes)) => bytes
+              case Right(bytes) => bytes
+            }.toArray
+
+            // write the file
+            fileStorage.writeAll(path, data)
+          }
+        }
       }
       .as(Empty())
   }
