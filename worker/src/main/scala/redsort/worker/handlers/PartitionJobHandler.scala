61a93fe2673e20a6a85c6ad75d382f84a4c49421
Quasar-Kim <vhfflrhs128@gmail.com>
2025-11-27 18:28:31 +0900

    fix: use ByteString instead of String to handle bytes in PartitionJobHandler

---
+++ worker/src/main/scala/redsort/worker/handlers/PartitionJobHandler.scala
@@ -0,0 +1,90 @@
+package redsort.worker.handlers
+
+import cats._
+import cats.effect._
+import cats.syntax.all._
+import fs2.{Chunk, Stream}
+import fs2.io.file.Path
+import redsort.jobs.Common._
+import redsort.jobs.messages._
+
+import redsort.jobs.context.interface._
+import redsort.jobs.worker._
+import com.google.protobuf.any
+import com.google.protobuf.any.{Any => ProtobufAny}
+
+import java.util.Arrays
+import cats.effect.std.Queue
+import java.io.File
+import com.google.protobuf.ByteString
+import scala.math.Ordering.Implicits._
+import fs2.io.file.Files
+import fs2.Pipe
+
+class PartitionJobHandler extends JobHandler {
+
+  private val RECORD_SIZE = 100 // 100 bytes
+  private val MIN_KEY = ByteString.fromHex("00000000000000000000")
+  private val MAX_KEY = ByteString.fromHex("0100000000000000000000")
+
+  implicit val byteStringComparator: Ordering[ByteString] =
+    Ordering.comparatorToOrdering(ByteString.unsignedLexicographicalComparator())
+
+  override def apply(
+      args: Seq[ProtobufAny],
+      inputs: Seq[Path],
+      outputs: Seq[Path],
+      ctx: FileStorage,
+      d: Directories
+  ): IO[Option[Array[Byte]]] = {
+
+    // pipe that writes chunk that is in `partition` to `path`.
+    def outputPipe(
+        path: Path,
+        partition: Tuple2[ByteString, ByteString]
+    ): Pipe[IO, Chunk[Byte], Unit] =
+      (inStream: Stream[IO, Chunk[Byte]]) => {
+        val sink = ctx.create(path.toString)
+        inStream
+          .filter { chunk =>
+            val key = ByteString.copyFrom(chunk.toArray.slice(0, 10))
+            isInPartition(key, partition)
+          }
+          .unchunks
+          .through(sink)
+      }
+
+    for {
+      _ <- assertIO(inputs.length == 1, "input length must be 1")
+      _ <- assertIO(outputs.length > 0, "output length must be longer than 0")
+
+      // Create output pipes for each output files
+      partitions = partitionsFromArgs(args)
+      outPipes = outputs.lazyZip(partitions).map { case (path, partition) =>
+        outputPipe(path, partition)
+      }
+
+      // Construct and run stream that reads from input file and partition it into outupt files
+      stream =
+        ctx
+          .read(inputs.head.toString)
+          .chunkN(RECORD_SIZE, allowFewer = false)
+          .broadcastThrough(outPipes: _*)
+      _ <- stream.compile.drain
+    } yield None
+  }
+
+  def partitionsFromArgs(args: Seq[ProtobufAny]): Seq[Tuple2[ByteString, ByteString]] = {
+    val ends = args.map(_.unpack[BytesArg].value)
+    ends.zipWithIndex.map { case (end, i) =>
+      val start = if (i == 0) MIN_KEY else ends(i - 1)
+      (start, end)
+    }.toSeq
+  }
+
+  def isInPartition(key: ByteString, p: Tuple2[ByteString, ByteString]): Boolean = {
+    val result = (key >= p._1) && (key < p._2)
+    result
+  }
+
+}
---
+++ worker/src/test/scala/redsort/handler/PartitionJobHandlerSpec.scala
@@ -0,0 +1,150 @@
+package redsort.worker.handlers
+
+import cats.effect._
+import cats.effect.testing.scalatest.AsyncIOSpec
+import cats.syntax.all._
+import fs2.{Pipe, Stream}
+import fs2.io.file.{Files, Path}
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.should.Matchers
+import redsort.jobs.Common.FileEntry
+import redsort.jobs.worker.Directories
+import redsort.worker.gensort.gensort
+
+import redsort.jobs.context._
+import redsort.jobs.context.interface._
+import redsort.jobs.context.impl._
+
+import com.google.protobuf.any
+import redsort.jobs.messages.BytesArg
+import com.google.protobuf.any.{Any => ProtobufAny}
+import com.google.protobuf.ByteString
+import scala.math.Ordering.comparatorToOrdering
+import scala.math.Ordering.Implicits._
+
+import redsort.worker.testctx._
+
+class PartitionJobHandlerSpec extends AsyncFlatSpec with AsyncIOSpec with Matchers {
+
+  val MAX_KEY = ByteString.fromHex("0100000000000000000000")
+
+  def makeArgFromByteString(bytes: ByteString): ProtobufAny = {
+    any.Any.pack(new BytesArg(value = bytes))
+  }
+
+  def fixture = new {
+    val records = Seq(
+      ByteString.fromHex("00000000000000000001").concat(ByteString.copyFromUtf8("a" * 88 + "\r\n")),
+      ByteString.fromHex("00000000000000000005").concat(ByteString.copyFromUtf8("b" * 88 + "\r\n")),
+      ByteString.fromHex("00000000000000000010").concat(ByteString.copyFromUtf8("c" * 88 + "\r\n")),
+      ByteString.fromHex("00000000000000000015").concat(ByteString.copyFromUtf8("d" * 88 + "\r\n")),
+      ByteString.fromHex("00000000000000000020").concat(ByteString.copyFromUtf8("e" * 88 + "\r\n"))
+    )
+    val inputContents = records.fold(ByteString.empty)((acc, s) => acc.concat(s))
+  }
+
+  "PartitionJobHandler" should "separates simple data in the partition (left inclusive)" in {
+    val f = fixture
+    val inputPathStr = "/data/input_simple"
+    val outputPathStrs = List("/data/partition1", "/data/partition2")
+    val inputPath = Path(inputPathStr)
+    val outputPaths = outputPathStrs.map(p => Path(p))
+    val dummyDirs: Directories = null
+    val argList = Seq(ByteString.fromHex("00000000000000000009"), MAX_KEY)
+    val args = argList.map(makeArgFromByteString)
+
+    for {
+      fs <- Ref.of[IO, Map[String, Array[Byte]]](Map.empty)
+      ctx = new WorkerTestCtx(fs)
+      partitioner = new PartitionJobHandler()
+      _ <- ctx.writeAll(inputPathStr, f.inputContents.toByteArray())
+
+      resultOpt <- partitioner.apply(
+        args = args,
+        inputs = Seq(inputPath),
+        outputs = outputPaths,
+        ctx = ctx,
+        d = dummyDirs
+      )
+
+      firstPartitionBytes <- ctx.readAll(outputPathStrs(0))
+      secondPartitionBytes <- ctx.readAll(outputPathStrs(1))
+
+      firstPartitionRecords = firstPartitionBytes.grouped(100).toArray
+      secondPartitionRecords = secondPartitionBytes.grouped(100).toArray
+
+    } yield {
+      firstPartitionBytes shouldBe (f.records(0).concat(f.records(1)).toByteArray())
+      secondPartitionBytes shouldBe (
+        f.records(2).concat(f.records(3)).concat(f.records(4)).toByteArray()
+      )
+    }
+  }
+
+  it should "create the file even if it contains no content" in {
+    val f = fixture
+    val inputPathStr = "/data/input"
+    val outputPathStrs = List("/data/partition1", "/data/partition2")
+    val inputPath = Path(inputPathStr)
+    val outputPaths = outputPathStrs.map(p => Path(p))
+    val dummyDirs: Directories = null
+    val argList = Seq(ByteString.fromHex("00000000000000000030"), MAX_KEY)
+    val args = argList.map(makeArgFromByteString)
+
+    for {
+      fs <- Ref.of[IO, Map[String, Array[Byte]]](Map.empty)
+      ctx = new WorkerTestCtx(fs)
+      partitioner = new PartitionJobHandler()
+      _ <- ctx.writeAll(inputPathStr, f.inputContents.toByteArray())
+
+      resultOpt <- partitioner.apply(
+        args = args,
+        inputs = Seq(inputPath),
+        outputs = outputPaths,
+        ctx = ctx,
+        d = dummyDirs
+      )
+
+      firstPartitionBytes <- ctx.readAll(outputPathStrs(0))
+      secondPartitionBytes <- ctx.readAll(outputPathStrs(1))
+
+      firstPartitionRecords = firstPartitionBytes.grouped(100).toArray
+      secondPartitionRecords = secondPartitionBytes.grouped(100).toArray
+
+    } yield {
+
+      firstPartitionBytes shouldBe (f.inputContents.toByteArray())
+      secondPartitionBytes shouldBe (Array.emptyByteArray)
+    }
+  }
+
+  it should "handle case where only one partition is given" in {
+    val f = fixture
+    val inputPathStr = "/data/input_simple"
+    val outputPathStrs = List("/data/partition1")
+    val inputPath = Path(inputPathStr)
+    val outputPaths = outputPathStrs.map(p => Path(p))
+    val dummyDirs: Directories = null
+    val argList = Seq(MAX_KEY)
+    val args = argList.map(makeArgFromByteString)
+
+    for {
+      fs <- Ref.of[IO, Map[String, Array[Byte]]](Map.empty)
+      ctx = new WorkerTestCtx(fs)
+      partitioner = new PartitionJobHandler()
+      _ <- ctx.writeAll(inputPathStr, f.inputContents.toByteArray())
+
+      resultOpt <- partitioner.apply(
+        args = args,
+        inputs = Seq(inputPath),
+        outputs = outputPaths,
+        ctx = ctx,
+        d = dummyDirs
+      )
+
+      firstPartitionBytes <- ctx.readAll(outputPathStrs(0))
+    } yield {
+      firstPartitionBytes shouldBe (f.inputContents.toByteArray())
+    }
+  }
+}
