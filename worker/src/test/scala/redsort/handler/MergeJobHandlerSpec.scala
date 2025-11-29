2964894c48097f44f95101b45a70331acf60bf88
Quasar Kim <35187730+Quasar-Kim@users.noreply.github.com>
2025-11-29 23:49:19 +0900

    fix: Fix MergeJobHandler failing to merge two files (#36)
    This PR includes completely rewritten version of MergeJobHandler which eliminates cryptic error that happen when you try to merge two unequal sized input files.
    
    Also fixes PseudoGensort generating only bytes in ASCII range (0~127), which made some tests failing to detect ordering problem caused by negative bytes.
    
    ----------
    
    * test: update state before emitting JobCompleted event to main fiber to make test to pass
    
    * fix: rework MergeJobHandler
    
    * fix: allow empty and only one input
    
    * fix: factor out `Record` into seperate file, with correct unsigned ordering
    
    * fix: update PseudoGensort to generate bytes, not strings

--- worker/src/test/scala/redsort/handler/MergeJobHandlerSpec.scala
+++ worker/src/test/scala/redsort/handler/MergeJobHandlerSpec.scala
@@ -17,10 +17,36 @@ import redsort.jobs.context.interface._
 import redsort.jobs.context.impl._
 
 import redsort.worker.testctx._
+import com.google.protobuf.ByteString
 
 class MergeJobHandlerSpec extends AsyncFlatSpec with AsyncIOSpec with Matchers {
+  def fixture = new {
+    def withCtx(body: (WorkerTestCtx) => IO[Unit]): IO[Unit] = {
+      for {
+        fs <- IO.ref[Map[String, Array[Byte]]](Map.empty)
+        ctx = new WorkerTestCtx(fs)
+        _ <- body(ctx)
+      } yield ()
+    }
+
+    def prepareSortedFile(path: Path, data: Array[Byte], ctx: FileStorage): IO[Path] = {
+      val tmpPath = Path("/tmp") / path
+      for {
+        _ <- ctx.writeAll(tmpPath.toString, data)
+        _ <- (new SortJobHandler).apply(
+          args = Seq.empty,
+          inputs = Seq(tmpPath),
+          outputs = Seq(path),
+          ctx = ctx,
+          d = null
+        )
+      } yield path
+    }
+  }
 
   "MergeJobHandler" should "exactly merge 10,000 records (1MB)" in {
+    val f = fixture
+
     val recordsPerFile = 10000
     val input1Data = gensort.generate(recordsPerFile)
     val input2Data = gensort.generate(recordsPerFile)
@@ -33,49 +59,129 @@ class MergeJobHandlerSpec extends AsyncFlatSpec with AsyncIOSpec with Matchers {
     val mergedoutputStr = "/data/mergedoutput"
     val dummyDirs: Directories = null
 
-    for {
-      fs <- Ref.of[IO, Map[String, Array[Byte]]](Map.empty)
-      ctx = new WorkerTestCtx(fs)
-      sorter = new SortJobHandler()
-      merger = new MergeJobHandler()
-      _ <- ctx.writeAll(sortinputStr, input1Data)
-      _ <- ctx.writeAll(sortinputStr2, input2Data)
+    f.withCtx { ctx =>
+      val sorter = new SortJobHandler()
+      val merger = new MergeJobHandler()
+      for {
+        _ <- ctx.writeAll(sortinputStr, input1Data)
+        _ <- ctx.writeAll(sortinputStr2, input2Data)
 
-      _ <- sorter.apply(
-        args = Seq.empty,
-        inputs = Seq(Path(sortinputStr)),
-        outputs = Seq(Path(sortoutputStr)),
-        ctx = ctx,
-        d = dummyDirs
-      )
-      _ <- sorter.apply(
-        args = Seq.empty,
-        inputs = Seq(Path(sortinputStr2)),
-        outputs = Seq(Path(sortoutputStr2)),
-        ctx = ctx,
-        d = dummyDirs
-      )
-      _ <- merger.apply(
-        args = Seq.empty,
-        inputs = Seq(Path(sortoutputStr), Path(sortoutputStr2)),
-        outputs = Seq(Path(mergedoutputStr)),
-        ctx = ctx,
-        d = dummyDirs
-      )
+        _ <- sorter.apply(
+          args = Seq.empty,
+          inputs = Seq(Path(sortinputStr)),
+          outputs = Seq(Path(sortoutputStr)),
+          ctx = ctx,
+          d = dummyDirs
+        )
+        _ <- sorter.apply(
+          args = Seq.empty,
+          inputs = Seq(Path(sortinputStr2)),
+          outputs = Seq(Path(sortoutputStr2)),
+          ctx = ctx,
+          d = dummyDirs
+        )
+        _ <- merger.apply(
+          args = Seq.empty,
+          inputs = Seq(Path(sortoutputStr), Path(sortoutputStr2)),
+          outputs = Seq(Path(mergedoutputStr)),
+          ctx = ctx,
+          d = dummyDirs
+        )
 
-      middleBytes1 <- ctx.readAll(sortoutputStr)
-      middleBytes2 <- ctx.readAll(sortoutputStr2)
+        middleBytes1 <- ctx.readAll(sortoutputStr)
+        middleBytes2 <- ctx.readAll(sortoutputStr2)
 
-      outputSize <- ctx.fileSize(mergedoutputStr)
-      outputBytes <- ctx.readAll(mergedoutputStr)
+        outputSize <- ctx.fileSize(mergedoutputStr)
+        outputBytes <- ctx.readAll(mergedoutputStr)
 
-    } yield {
-      outputSize shouldBe (recordsPerFile * 2 * 100) // 200 records, 100 bytes each
-      outputBytes.takeRight(2) shouldBe Array('\r'.toByte, '\n'.toByte)
-      gensort.validate(middleBytes1) shouldBe true
-      gensort.validate(middleBytes2) shouldBe true
-      gensort.validate(outputBytes) shouldBe true
-      outputBytes.grouped(100).map(_.toList).toSet shouldBe data
+      } yield {
+        outputSize shouldBe (recordsPerFile * 2 * 100) // 200 records, 100 bytes each
+        outputBytes.takeRight(2) shouldBe Array('\r'.toByte, '\n'.toByte)
+        gensort.validate(middleBytes1) shouldBe true
+        gensort.validate(middleBytes2) shouldBe true
+        gensort.validate(outputBytes) shouldBe true
+        outputBytes.grouped(100).map(_.toList).toSet shouldBe data
+      }
+    }
+  }
+
+  it should "handle empty input" in {
+    val f = fixture
+
+    f.withCtx { ctx =>
+      for {
+        _ <- (new MergeJobHandler).apply(
+          args = Seq.empty,
+          inputs = Seq.empty,
+          outputs = Seq.empty,
+          ctx = ctx,
+          d = null
+        )
+      } yield ()
+    }
+  }
+
+  it should "handle only one input file" in {
+    val f = fixture
+    val inputData = gensort.generate(100)
+
+    f.withCtx { ctx =>
+      for {
+        // prepare input file by sorting input files
+        _ <- f.prepareSortedFile(Path("/input/a"), inputData, ctx)
+
+        _ <- (new MergeJobHandler).apply(
+          args = Seq(),
+          inputs = Seq(
+            Path("/input/a")
+          ),
+          outputs = Seq(
+            Path("/output/b")
+          ),
+          ctx = ctx,
+          d = null
+        )
+
+        contents <- ctx.readAll("/output/b")
+        expectedContents <- ctx.readAll("/input/a")
+      } yield {
+        contents shouldBe expectedContents
+      }
+    }
+  }
+
+  // regression: https://github.com/Quasar-Kim/332project/issues/34
+  it should "merge two files with sizes 5500 and 4500" in {
+    val f = fixture
+    val input1Data = gensort.generate(55)
+    val input2Data = gensort.generate(45)
+    val mergedData = input1Data.concat(input2Data)
+
+    f.withCtx { ctx =>
+      for {
+        // prepare input file by sorting input files
+        _ <- f.prepareSortedFile(Path("/input/a"), input1Data, ctx)
+        _ <- f.prepareSortedFile(Path("/input/b"), input2Data, ctx)
+        _ <- f.prepareSortedFile(Path("/sorted"), mergedData, ctx)
+
+        _ <- (new MergeJobHandler).apply(
+          args = Seq(),
+          inputs = Seq(
+            Path("/input/a"),
+            Path("/input/b")
+          ),
+          outputs = Seq(
+            Path("/output/c")
+          ),
+          ctx = ctx,
+          d = null
+        )
+
+        contents <- ctx.readAll("/output/c")
+        expectedContents <- ctx.readAll("/sorted")
+      } yield {
+        ByteString.copyFrom(contents) shouldBe ByteString.copyFrom(expectedContents)
+      }
     }
   }
 }
