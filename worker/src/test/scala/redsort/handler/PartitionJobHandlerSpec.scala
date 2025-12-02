a1d6169e512fa97d8b16f299f0f90368bd97e721
Jaehwan Kim <me@jaehwan.kim>
2025-12-02 15:58:39 +0900

    test: add benchmark for throughput with 500k records in PartitionJobHandlerSpec

--- worker/src/test/scala/redsort/handler/PartitionJobHandlerSpec.scala
+++ worker/src/test/scala/redsort/handler/PartitionJobHandlerSpec.scala
@@ -217,4 +217,81 @@ class PartitionJobHandlerSpec extends AsyncFlatSpec with AsyncIOSpec with Matche
       compareKeys(lastOfFirst, firstOfSecond) shouldBe true
     }
   }
+
+  it should "benchmark throughput with 500k records (~50MB)" in {
+    val numRecords = 500000
+    val recordSize = 100
+    val keySize = 10
+    val commonPayload = ("x" * 88 + "\r\n").getBytes("UTF-8")
+    val totalSize = numRecords * recordSize
+
+    val inputBytes = new Array[Byte](totalSize)
+    val buffer = java.nio.ByteBuffer.wrap(inputBytes)
+
+    for (i <- 0 until numRecords) {
+      val keyBytes = new Array[Byte](keySize)
+      var value = i
+      var k = keySize - 1
+      while (value > 0 && k >= 0) {
+        keyBytes(k) = (value & 0xff).toByte
+        value >>= 8
+        k -= 1
+      }
+      buffer.put(keyBytes)
+      buffer.put(commonPayload)
+    }
+
+    val inputPathStr = "/data/input_bench"
+    val outputPathStrs = List("/data/part_bench_1", "/data/part_bench_2")
+    val outputPaths = outputPathStrs.map(Path(_))
+    val dummyDirs: Directories = null
+
+    val midBytes = new Array[Byte](keySize)
+    var midVal = numRecords / 2
+    var k = keySize - 1
+    while (midVal > 0 && k >= 0) {
+      midBytes(k) = (midVal & 0xff).toByte
+      midVal >>= 8
+      k -= 1
+    }
+
+    val midKeyArg = makeArgFromByteString(ByteString.copyFrom(midBytes))
+    val args = Seq(midKeyArg, makeArgFromByteString(MAX_KEY))
+
+    for {
+      fs <- Ref.of[IO, Map[String, Array[Byte]]](Map.empty)
+      ctx = new WorkerTestCtx(fs)
+      partitioner = new PartitionJobHandler()
+
+      _ <- ctx.writeAll(inputPathStr, inputBytes)
+      _ <- IO.println(
+        s"Starting benchmark with ${numRecords} records (${totalSize / 1024 / 1024} MB)..."
+      )
+
+      start <- IO(System.nanoTime())
+      _ <- partitioner.apply(args, Seq(Path(inputPathStr)), outputPaths, ctx, dummyDirs)
+      end <- IO(System.nanoTime())
+
+      part1 <- ctx.readAll(outputPathStrs(0))
+      part2 <- ctx.readAll(outputPathStrs(1))
+    } yield {
+      val durationSeconds = (end - start) / 1e9
+      val throughputMBps = (totalSize / 1024.0 / 1024.0) / durationSeconds
+
+      println(f"Benchmark Finished in $durationSeconds%.4f s")
+      println(f"Throughput: $throughputMBps%.2f MB/s")
+
+      (part1.length + part2.length) shouldBe totalSize
+      part1.length should be > 0
+      part2.length should be > 0
+      compareKeys(
+        part1.takeRight(100).take(10).toArray,
+        midBytes
+      ) shouldBe true
+      compareKeys(
+        midBytes,
+        part2.take(100).take(10).toArray
+      ) shouldBe true
+    }
+  }
 }
