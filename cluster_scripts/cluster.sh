82d19cb4151c7c6ba55273091470b989f02a71a6
DanaKharaz <danakharaz@gmail.com>
2025-12-03 14:48:38 +0900

    test: improve test scripts

--- cluster_scripts/cluster.sh
+++ cluster_scripts/cluster.sh
@@ -5,175 +5,79 @@ WORKER_IPS=("2.2.2.101" "2.2.2.102" "2.2.2.103" "2.2.2.104" "2.2.2.105" "2.2.2.1
 WORKER_SSH_PORT=22
 USER="red"
 DIR_PROJECT="/home/red/332project"
-# TODO customize directories (?)
-DIRS_INPUT=("/home/red/input1" "/home/red/input2")
-DIR_OUTPUT="/home/red/data"
 GIT_REPO="https://github.com/Quasar-Kim/332project"
-GENSORT_PATH="/home/red/gensort"
 VALSORT_PATH="/home/red/valsort"
 
 ##### UPDATE PROJECT #####
 update_master() {
     echo "[START] updating project code on master"
-    git clone $GIT_REPO 2>/dev/null || echo 'already cloned'
     # compile jobs and master
-    cd 332project && git pull origin main && sbt jobs/compile && sbt master/compile
+    cd 332project && git checkout main && git pull && sbt jobs/compile && sbt master/compile && sbt master/assembly
     echo "[DONE]"
 }
 update_workers() {
     local n=$1
-    echo "[START] updating project code on $n workers"
+    echo "[START] updating, compiling, and assembling project code on $n workers"
     for worker in ${WORKER_IPS[@]:0:$n}; do
         echo $worker;
-        # clone
-        ssh $USER@$worker -p $WORKER_SSH_PORT \
-            "clone $GIT_REPO 2>/dev/null || echo 'already cloned'"
         # update
-        ssh $USER@$worker -p $WORKER_SSH_PORT \
-            "cd $DIR_PROJECT && git pull origin main && sbt jobs/compile && sbt worker/compile"
+        ssh -f $USER@$worker -p $WORKER_SSH_PORT \
+            "cd $DIR_PROJECT && git checkout main && git pull && sbt jobs/compile && sbt worker/compile && sbt worker/assembly && echo $worker done"
     done
     echo "[DONE]"
 }
-
-##### GENERATE DATA #####
-init_gensort() {
+update_specific_worker() {
     local n=$1
-    echo "[START] initializing gensort on master"
-    if [ -f $GENSORT_PATH ]; then
-        echo "gensort already initialized"
-    else
-        cd /home/$USER
-        wget http://www.ordinal.com/try.cgi/gensort-linux-1.5.tar.gz # TODO put the binaries on git?
-        tar -xzf gensort-linux-1.5.tar.gz
-        cp /home/$USER/64/* /home/$USER # gensort and valsort
-    fi
-    echo "[DONE]"
-    echo "[START] copying gensort to $n workers"
-    for worker in ${WORKER_IPS[@]:0:$n}; do
-        echo $worker;
-        scp -P $WORKER_SSH_PORT $GENSORT_PATH $worker:/home/$USER
-        scp -P $WORKER_SSH_PORT $VALSORT_PATH $worker:/home/$USER
-    done
-    echo "[DONE]"
-}
-make_dirs() {
-    local n=$1
-    echo "[START] making input and output directories on $n workers"
-    for worker in ${WORKER_IPS[@]:0:$n}; do
-        echo $worker;
-
-        # output
-        # delete old directory
-        ssh $USER@$worker -p $WORKER_SSH_PORT \
-            "rm -rf $DIR_OUTPUT"
-        # make new directory
-        ssh $USER@$worker -p $WORKER_SSH_PORT \
-            "mkdir -p $DIR_OUTPUT"
-
-        # input
-        for dir_inp in ${DIRS_INPUT[@]}; do
-            # delete old directory
-            ssh $USER@$worker -p $WORKER_SSH_PORT \
-                "rm -rf $dir_inp"
-            # make new directory
-            ssh $USER@$worker -p $WORKER_SSH_PORT \
-                "mkdir -p $dir_inp"
-        done
-    done
-    echo "[DONE]"
-}
-# TODO different number of records per file, different number of files per worker
-generate_data() {
-    local n=$1 # number of workers
-    local records=$2 # to generate on each worker
-    #local ascii=$3 # 1=ascii 0=binary # TODO?
-    # TODO add a start for the records?
-    local n_files=$3
-    echo "[START] generating $records records to $n_files files on $n workers"
-    for worker in ${WORKER_IPS[@]:0:$n}; do
-        echo $worker;
-        for ((i=1; i<=n_files; i++)); do
-            for dir_inp in ${DIRS_INPUT[@]}; do
-                path="$dir_inp/data$i"
-                # delete old record
-                ssh $USER@$worker -p $WORKER_SSH_PORT \
-                    "if [ -f $path ]; then rm $path; fi"
-                # generate new record
-                ssh $USER@$worker -p $WORKER_SSH_PORT \
-                    "$GENSORT_PATH $records $path"
-            done
-        done
-    done
+    echo "[START] updating, compiling, and assembling project code on worker $n"
+    worker=${WORKER_IPS[$(( n - 1 ))]}
+    echo $worker
+    # update
+    ssh $USER@$worker -p $WORKER_SSH_PORT \
+        "cd $DIR_PROJECT && git checkout main && git pull && sbt jobs/compile && sbt worker/compile && sbt worker/assembly"
     echo "[DONE]"
 }
 
-##### START MASTER #####
-start_master() {
+##### INITIALIZE A WORKER #####
+init_specific_worker() {
     local n=$1
-    echo "[START] starting master for $n workers"
-    ( cd "$DIR_PROJECT" && sbt "master/run $n" )
-}
-
-##### START WORKERS #####
-start_workers() {
-    local n=$1
-    local master_ip=$2
-    local master_port=$3
-    # TODO different input directories per worker
-    input_dirs_str=$(IFS=" "; echo "${DIRS_INPUT[*]}") # all same on each worker for now
-    echo input_dirs_str
-    echo "[START] starting $n workers"
-    for worker in ${WORKER_IPS[@]:0:$n}; do
-        echo $worker;
-        # clear the output directory
-        ssh $USER@$worker -p $WORKER_SSH_PORT \
-            "rm -rf $DIR_OUTPUT/*"
-        # start worker
-        ssh $USER@$worker -p $WORKER_SSH_PORT \
-            "( cd $DIR_PROJECT; sbt 'worker/run $master_ip:$master_port -I $input_dirs_str -O $DIR_OUTPUT' )"
-    done
+    echo "[START] initializing worker $n (establishing a connection, cloning the repository)"
+    worker=${WORKER_IPS[$(( n - 1 ))]}
+    echo $worker
+    # update
+    ssh $USER@$worker -p $WORKER_SSH_PORT \
+        "git clone $GIT_REPO"
+    # add valsort
+    scp -P $WORKER_SSH_PORT $VALSORT_PATH $worker:/home/$USER
     echo "[DONE]"
-}
-
-##### CHECK RESULTS #####
-# TODO
-
-##### ENTIRE PROJECT #####
-# update all, generate data, run master
-prepare() {
-    local n=$1
-    local records=$2 # to generate on each worker
-    local n_files=$3 # to generate in input directories
-    update_master
-    update_workers $n
-    init_gensort $n
-    make_dirs $n
-    generate_data $n $records $n_files
 }
 
 usage() {
     echo "Commands:"
-    echo "1) $0 prepare <# workers> <# records per files> <# files per worker> -- updates code, generates data"
-    echo "2) $0 start_master <# workers>"
-    echo "3) $0 start_workers <# workers> -- should be done after starting the master"
+    echo "1) $0 update <# workers> -- updates, compiles, and assembles code on master and the specified number of workers"
+    echo "2) $0 update_specific <idx of worker> -- updates, compiles, and assembles code on the specified worker, i.e., vm0[idx] or vm[idx]"
+    echo "3) $0 init_specific <idx of workers> -- initializes the specified worker (clones the repository, copies valsort, compiles, and assembles)"
     echo ""
     echo "Number of workers: 1-20"
     echo ""
     echo "Examples:"
-    echo "1) $0 prepare 3 10000 2 -- will generate 10000 in 2 files on 3 worker machnines"
+    echo "1) $0 update 3 -- will update master, vm01, vm02, and vm03"
+    echo "2) $0 update_specific 4 -- will update vm04"
+    echo "3) $0 init_specific 5 -- will initialize and update vm05"
 }
 
 case $1 in
-  prepare)
-    prepare $2 $3 $4 # # of workers, # of records to generate in each file, # of files to generate on each worker
+  update)
+    update_master
+    update_workers $2 # # of workers
     ;;
-  start_master)
-    start_master $2 # number of workers
+  update_specific)
+    update_specific_worker $2 # idx of worker
     ;;
-  start_workers)
-    start_workers $2 "10.1.25.21" 5000 # # of workers
+  init_specific)
+    init_specific_worker $2 # idx of worker
+    update_specific_worker $2 # idx of worker
     ;;
-  *) # Default case
+  *)
     usage
     ;;
 esac
